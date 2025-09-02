5cc1198348c8ad8c89c91f5307063b2c424662ff
spire <spire47911@yandex.ru>
2025-09-02 15:59:25 +0500

    commit #1

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "org.writer"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+    withSourcesJar()
+    withJavadocJar()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "csv"
+            pom {
+                name.set("csv")
+                description.set("CSV utilities")
+            }
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
